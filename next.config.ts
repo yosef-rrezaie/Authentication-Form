55f4f71c8d4aa341207402b8784ccc618853eb84
Yosef Rezaie <yosefrrezaie@gmail.com>
2025-09-13 07:34:59 +0330

    .

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        pathname: "/api/portraits/**",
-      },
-    ],
+  iimages: {
+    domains: ["randomuser.me"],
   },
 };
 
